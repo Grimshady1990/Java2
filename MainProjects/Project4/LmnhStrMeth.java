73ae9b4e87cab3c08f90c0d57a2f37e335300b33
Grimshady1990 <frankiedthomas@gmail.com>
2025-02-15 12:58:56 +0000

    MOBILE DEVICE: added instructions in the comments, and created 2 varibles

---
+++ MainProjects/Project4/LmnhStrMeth.java
@@ -0,0 +1,15 @@
+// We need to showcase different string methods
+// we need to join two strings
+// make a string AllCaps
+// make a string lower case
+// decide if a string is equal to another
+// can it be equal and ignore caps
+// find the index of a given character.
+// find a charcter at a given index
+// create a sub string
+
+public class LmnhStrMeth {
+
+    String baseString = "I am a silly string";
+    String concatMe = ", but only on Saturdays"
+}
