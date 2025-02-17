c774c35e275018d55057d813023918aec9e55ad3
Grimshady1990 <frankiedthomas@gmail.com>
2025-02-17 16:42:19 +0000

    DESKTOP DEVICE: created Lmnh files for Project 6

---
+++ MainProjects/Project6/LmnhNoodle.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class LmnhNoodle {
+
+    protected double lengthInCentimeters;
+    protected double widthInCentimeters;
+    protected String shape;
+    protected String ingredients;
+
+    public LmnhNoodle(double lenInCent, double wthInCent, String shp, String ingr) {
+
+        this.lengthInCentimeters = lenInCent;
+        this.widthInCentimeters = wthInCent;
+        this.shape = shp;
+        this.ingredients = ingr;
+    }
+
+    public String toString(String noodleName) {
+        return "does not matter as this method will be overridden.";
+    }
+
+    public String 
+
+}
